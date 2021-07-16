7cb7319570d8951bf72fca72f25e6c417766ab14
tingtt <t.t.launcher.real@gmail.com>
2021-07-16 15:56:35 +0900

    [add] MySQLのマイグレーション用SQL

---
+++ db/mysql/init/insert_data.sql
@@ -0,0 +1,21 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Create table
+
+CREATE TABLE `users` (
+    `id` bigint UNSIGNED NOT NULL,
+    `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ユーザの表示名',
+    `age`int UNSIGNED NOT NULL COMMENT 'ユーザーの年齢',
+    `gender` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ユーザの性別',
+)
+
+-- Insert sample datas
+
+INSERT INTO `users` (`id`, `name`, `age`, `gender`) VALUES
+(1, 'user1', '20', '男'),
+(2, 'user2', '20', '女'),
+(3, 'user3', '20', '男'),
+(4, 'user4', '20', '女'),
